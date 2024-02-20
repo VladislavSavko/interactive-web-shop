e112d8953c630608b2a397a5d8f678597d992af7
VladislavSavko <vlad.savko.2003@inbox.ru>
2024-02-20 22:27:50 +0300

    added Eureka module + Gateway module

---
+++ gateway-service/src/main/java/com/vlados/webshop/gatewayservice/config/RouteConfiguration.java
@@ -0,0 +1,20 @@
+package com.vlados.webshop.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(
+                        "eureka-server",
+                        r -> r.path("/eureka/**")
+                                .uri("lb://eureka-server")
+                )
+                .build();
+    }
+}
