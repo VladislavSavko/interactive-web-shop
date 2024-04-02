ce1fd74bb79de3c53f1237f4f78372a3a42e8fc7
VladislavSavko <vlad.savko.2003@inbox.ru>
2024-04-02 15:46:09 +0300

    user-service exceptions fixed

---
+++ user-service/src/main/java/com/vlados/webshop/userservice/controller/advice/UserExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.vlados.webshop.userservice.controller.advice;
+
+import com.vlados.webshop.userservice.dto.exception.ExceptionResponse;
+import com.vlados.webshop.userservice.exception.NoSuchEntityException;
+import com.vlados.webshop.userservice.exception.WrongCredentialsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class UserExceptionHandler {
+    @ExceptionHandler(NoSuchEntityException.class)
+    public ResponseEntity<ExceptionResponse> handleNoSuchEntityException(NoSuchEntityException nsee) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(nsee.getMessage(), LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(WrongCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleWrongCredentialsException(WrongCredentialsException wce) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(wce.getMessage(), LocalDateTime.now()));
+    }
+}
