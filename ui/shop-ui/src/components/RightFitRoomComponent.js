beafc29fb97efd33fb006398e9c2f37bafc63241
VladislavSavko <vladislav.savko@specific-group.com>
2024-05-06 15:57:14 +0300

    struggles with files dropzone

--- ui/shop-ui/src/components/RightFitRoomComponent.js
+++ ui/shop-ui/src/components/RightFitRoomComponent.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {FileUploader} from "react-drag-drop-files";
+import Dropzone from "react-dropzone";
 
 class RightFitRoomComponent extends React.Component {
     constructor(props) {
         super();
         this.state = {
             file: null,
-            types: ["JPEG", "PNG", "JPG"]
+            types: ["image/jpeg", "image/jpg", "image/png"]
         }
     }
 
@@ -17,15 +18,31 @@ class RightFitRoomComponent extends React.Component {
         })
     };
 
+    dragAndDropAreaStyle = () => {
+        return <div className="drag-and-drop-area">
+            <span>Sosi</span>
+        </div>
+    }
+
 
     render() {
         return <>
-            <FileUploader
-                handleChange={this.handleChange}
-                name="file"
-                types={this.state.types}
-                classes="image-file-uploader"
-            />
+            {/*<FileUploader*/}
+            {/*    handleChange={this.handleChange}*/}
+            {/*    name="file"*/}
+            {/*    types={this.state.types}*/}
+            {/*    // children={this.dragAndDropAreaStyle()}*/}
+            {/*/>*/}
+            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)} maxFiles={1} >
+                {({getRootProps, getInputProps}) => (
+                    <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <div style={{height: '600px'}}>Drag 'n' drop some files here, or click to select files</div>
+                        </div>
+                    </section>
+                )}
+            </Dropzone>
         </>
     }
 }
